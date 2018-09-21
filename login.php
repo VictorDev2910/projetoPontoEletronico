711a09d6fd03a08da26038589f7972bae2de5d4a
Victor <victordesenv@outlook.com>
2018-09-21 15:29:44 -0300

    Subindo repositorio

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">   
+    <title>Lan Solver</title> 
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.css.map">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css.map">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.css.map">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="css/login.css">
+    <style type="text/css">body{background: url() no-repeat;}</style>
+</head>
+<body>
+    <div class="x_content login">       
+        <div class="x_content" style="text-align:center; margin: 0 auto;">
+            <form class="form-horizontal" style="width: 300px; margin: 0 auto;" method="post" action="/form/">
+                <fieldset>
+                    <h1 class="text-center" id="login">Teste v 1.0</h1>
+                    <br>
+                    <input id="usuario" type=text class="form-control input-underline" placeholder=Usuário>
+                    <br>
+                    <input id="senha"  type=password class="form-control input-underline" placeholder=Senha>
+                </fieldset>  
+            </form>    
+        </div>
+    </div>
+</body>
+</html>
